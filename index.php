d4bee0ce6990211fa3585f10cdaac6280660bd66
Harshit Sonawala <hssonawala@gmail.com>
2022-10-06 15:39:12 +0530

    added animations

--- index.php
+++ index.php
@@ -8,6 +8,13 @@
     <div class="header">
       <h2 class="title">Care For You</h2>
     </div>
+    <div class="carousel">
+      <img class="carousel_image" src="assets\images\stock_image1.jpg" alt="">
+      <img class="carousel_image" src="assets\images\stock_image2.jpg" alt="">
+      <img class="carousel_image" src="assets\images\stock_image3.jpg" alt="">
+      <img class="carousel_image" src="assets\images\stock_image4.jpg" alt="">
+      <img class="carousel_image" src="assets\images\stock_image5.jpg" alt="">
+    </div>
     <div class="center80">
       <div class="card">
         <h2>Life got you down? Don't worry, you can always get back up.</h2>
