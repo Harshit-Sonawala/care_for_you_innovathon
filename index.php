78be1273c3460e0aff9a24b4cbb916065142d5df
Harshit Sonawala <hssonawala@gmail.com>
2022-10-06 14:23:19 +0530

    init

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Care For You</title>
+  </head>
+  <body>
+    <div>
+      <h3>Care For You</h3>
+    </div>
+  </body>
+</html>
