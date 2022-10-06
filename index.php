17ee9ac2a961b9d5b76f328e6a385aa530d82ead
Harshit Sonawala <hssonawala@gmail.com>
2022-10-06 15:06:23 +0530

    added styling

--- index.php
+++ index.php
@@ -5,8 +5,22 @@
     <title>Care For You</title>
   </head>
   <body>
-    <div>
-      <h3>Care For You</h3>
+    <div class="header">
+      <h2 class="title">Care For You</h2>
+    </div>
+    <div class="center80">
+      <div class="card">
+        <h2>Life got you down? Don't worry, you can always get back up.</h2>
+        <p>Its okay. We've all been there.</p>
+      </div>
+      <div class="card">
+        <h2>Find experts to guide you through this state of mind.</h2>
+        <p>Its okay. We've all been there.</p>
+      </div>
+      <div class="card">
+        <h2>Easily connect with the help you need.</h2>
+        <p>Its okay. We've all been there.</p>
+      </div>
     </div>
   </body>
 </html>
